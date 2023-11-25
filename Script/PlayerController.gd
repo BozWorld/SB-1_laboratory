ef3d76bead389b9c9c419cdf7d3a3a8b6afad041
Moeva <mondohamoeva@hotmail.fr>
2023-11-25 11:26:38 +0100

    { working on tileset and basics Camera }

--- Script/PlayerController.gd
+++ Script/PlayerController.gd
@@ -1,13 +1,18 @@
 extends CharacterBody2D
 
-var wheel_base = 12
-var steering_angle = 3
-
+var wheel_base = 10
+var steering_angle = 10
 var steer_direction
 
+var enginer_power = 900
+
+var acceleration = Vector2.ZERO
+
 func _physics_process(delta):
+	acceleration = Vector2.ZERO
 	get_input()
 	calculate_steering(delta)
+	velocity += acceleration * delta 
 	move_and_slide()
 
 func get_input():
@@ -15,12 +20,26 @@ func get_input():
 	steer_direction = turn * deg_to_rad(steering_angle)
 	velocity = Vector2.ZERO
 	if Input.is_action_pressed("fly"):
-		velocity = transform.y * 300
+		acceleration = transform.y * enginer_power
+		
+#func calculate_steering(delta):
+#	var rear_wheel = position + transform.y * wheel_base /2.0
+#	var front_wheel = position - transform.y * wheel_base / 2.0
+#	rear_wheel += velocity * delta
+#	front_wheel += velocity.rotated(steer_direction) * delta
+#	var new_heading = rear_wheel.direction_to(front_wheel)
+#	velocity = new_heading * velocity.length()
+#	rotation = new_heading.angle()
+	
 func calculate_steering(delta):
-	var rear_wheel = position - transform.y * wheel_base /2.0
+	# 1. Find the wheel positions
+	var rear_wheel = position - transform.y * wheel_base / 2.0
 	var front_wheel = position + transform.y * wheel_base / 2.0
+	# 2. Move the wheels forward
 	rear_wheel += velocity * delta
 	front_wheel += velocity.rotated(steer_direction) * delta
+	# 3. Find the new direction vector
 	var new_heading = rear_wheel.direction_to(front_wheel)
+	# 4. Set the velocity and rotation to the new direction
 	velocity = new_heading * velocity.length()
 	rotation = new_heading.angle()
