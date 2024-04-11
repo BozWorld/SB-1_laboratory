d22b2f49be49fdb8b4d2945fe16bf3e5a73deb39
ffdecaesteker <118174383+EDK-pro@users.noreply.github.com>
2024-04-11 18:53:39 +0200

    Elise a tout casse a ce que vous avez fait (rajout de phantom camera, de feedback et de juice)

--- Script/basics_actor/actor_controller/PlayerController.gd
+++ Script/basics_actor/actor_controller/PlayerController.gd
@@ -4,26 +4,31 @@ var acceleration : Vector2 = Vector2.ZERO
 var frictionMag: float = 60
 var friction: Vector2 = Vector2.ZERO
 var force : Vector2 = Vector2.ZERO
-var gravity : Vector2 = Vector2(0,0)
-var maxSpeed : Vector2 = Vector2(-80,-80)
-var minSpeed : Vector2 = Vector2(80,80)
-var AngularAcceleration : float = 50
+var gravity : Vector2 = Vector2(0,20)
+var maxSpeed : Vector2 = Vector2(-400,-300)
+var minSpeed : Vector2 = Vector2(400,300)
+var AngularAcceleration : float = 800
 var AngularVelocity: float = 0
-var minVAcceleration: float = -10
-var maxHAcceleration : float = 10
-var mass : float = 0.4
+var minVAcceleration: float = -7.0
+var maxHAcceleration : float = 7.0
+var mass : float = 0.6
 var time : float = 0
-var engine_power = -60  # Forward acceleration force.
-
+var engine_power = -200  # Forward acceleration force.
+var dir = Vector2.ZERO
 
 func _physics_process(delta):
 	var turn = Input.get_axis("rotate_left", "rotate_right")
 	AngularVelocity += deg_to_rad(AngularAcceleration* delta)
-	var dir = (position - $Node2D.global_position).normalized()
-	clampf(AngularVelocity,minVAcceleration,maxHAcceleration)
+	if turn == 0:
+		AngularVelocity = 0
+	dir = (position - $Node2D.global_position).normalized()
+	#print(dir)
+	AngularVelocity = clampf(AngularVelocity,minVAcceleration,maxHAcceleration)
 	rotation +=   turn * AngularVelocity * delta
+	clampf(rotation,minVAcceleration,maxHAcceleration)
 	get_input()
 	applyDrag()
+	applyFriction()
 	applyForces(gravity)
 	if Input.is_action_pressed("fly"):
 		force = Vector2(engine_power,engine_power) *dir
@@ -31,8 +36,9 @@ func _physics_process(delta):
 		force = Vector2(0,0)
 	applyForces(force)
 	velocity += acceleration * delta
+	checkEdgesBounce()
 	limitVelocity(maxSpeed,minSpeed)
-
+	
 	acceleration = Vector2.ZERO
 	position += velocity * delta
 	move_and_slide()
@@ -52,7 +58,24 @@ func applyDrag():
 	
 func applyForces(force: Vector2):
 	var f = Vector2(force/mass)
-	acceleration += f 
+	acceleration += f
+	
+## Permet de limiter les mouvements de l'avion 
+func checkEdgesBounce():
+	var limit = 2000
+	if((position.y > limit )):
+		velocity.y *= -2
+		position.y = limit
+	if((position.y < -limit )):
+		velocity.y *= -2
+		position.y = -limit
+	if((position.x > limit )):
+		velocity.x *= -2
+		position.x = limit
+	if((position.x < -limit )):
+		velocity.x *= -2
+		position.x = -limit
+
 func get_input():
 	
 	var turn = Input.get_axis("rotate_left", "rotate_right")
--- Script/debug_hud/DebugPhysicsHUD.gd
+++ Script/debug_hud/DebugPhysicsHUD.gd
@@ -1,6 +1,5 @@
 extends Node2D
 
-
 @export var masslabel : Label
 @export var frictionlabel : Label
 @export var pushforcelabel : Label
@@ -17,9 +16,8 @@ extends Node2D
 @export var gravity : Vector2
 @export var acceleration : Vector2
 
-
-
 func _physics_process(delta: float) -> void:
+	
 	mass = player.mass
 	velocity = player.velocity
 	acceleration = player.acceleration
@@ -30,3 +28,11 @@ func _physics_process(delta: float) -> void:
 	velocitylabel.text = "Velocity :  " +  str(velocity)
 	gravitylabel.text = "gravity : " +  str(gravity)
 	normallabel.text = "acceleration : " +  str(acceleration)
+	if player.velocity.length() >= 400.0:
+		$PcamTween.set_follow_target_offset(Vector2(0,0))
+		$PcamTween.set_follow_damping_value(6.0)
+
+	else:
+		$PcamTween.set_follow_target_offset(Vector2(-1 * $Player_scene.dir.x, -1 * $Player_scene.dir.y) * 300.0)
+		$PcamTween.set_follow_damping_value(1.0)
+
