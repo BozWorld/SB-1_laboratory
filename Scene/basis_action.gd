9354fe7e17adab08cebbb376c1c96d3ab47cc883
BozWorld <mondohamoeva@hotmail.fr>
2024-10-25 00:20:17 +0200

    {continue testing}

--- Scene/basis_action.gd
+++ Scene/basis_action.gd
@@ -1,16 +1,14 @@
-extends Node3D
+@tool
+extends MeshInstance3D
 
-
+@export var test: Basis 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if Input.is_action_pressed("left"):
-		rotation.z += deg_to_rad(-0.4)
-	if Input.is_action_pressed("right"):
-		rotation.z += deg_to_rad(0.4)
-	rotation.z = clamp(rotation.z,deg_to_rad(-12),deg_to_rad(12))
+	transform.basis = test
 	pass
