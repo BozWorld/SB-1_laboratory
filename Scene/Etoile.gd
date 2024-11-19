3fee47158a9290d9dcbad190a6568a8b7bd39c4c
Sammouille <77251950+Sammouille@users.noreply.github.com>
2024-11-19 11:40:55 +0100

    couleurs et grande ourse

--- Scene/Etoile.gd
+++ Scene/Etoile.gd
@@ -1,10 +1,20 @@
 @tool
 extends Node2D
 
-@export var frequence : float = 6.0
-@export var octave : float = 3.0
+@export var frequence : float = 6.0:
+	set(nouvelle_frequence): 
+		taille = 1.0 + (1.0/(octave*12.0 + nouvelle_frequence))*100
+		frequence = nouvelle_frequence
+@export var octave : float = 3.0:
+	set(nouvelle_octave): 
+		taille = 1.0 + (1.0/(nouvelle_octave*12.0 + frequence))*100
+		octave = nouvelle_octave
+		
 
+@export var couleur : Color = Color(1.0,1.0,1.0)
 @onready var sprite = $Sprite2D
+
+var taille : float
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -14,8 +24,13 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	if Engine.is_editor_hint():
 		editor_process(delta)
+		queue_redraw()
 
+func _draw():
+	draw_circle(Vector2(0,0), taille, couleur, true)
 
 func editor_process(delta):
-	sprite.scale = Vector2(1.0 + (1.0/(octave*12.0 + frequence))*5.0,1.0 + (1.0/(octave*12.0 + frequence))*5.0)
+	pass
+	
+	
 	
