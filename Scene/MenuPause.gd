4eb32297f51afe3f48a132c0ebf3ff0ea86499cd
Sammouille <77251950+Sammouille@users.noreply.github.com>
2024-11-24 17:59:45 +0100

    J'ai avancé le menu globalement mais jsp si tout est incr dedans

--- Scene/MenuPause.gd
+++ Scene/MenuPause.gd
@@ -6,6 +6,13 @@ const FAIT_PAUSE = false
 
 var ancien_mouse_mode
 
+var main 
+
+@onready var menu_central = find_child("MenuCentral")
+
+var scene_options = preload("res://Scene/menu_options.tscn")
+var inst_options
+
 @onready var anim_player : AnimationPlayer = $AnimationPlayer
 
 func _ready() -> void:
@@ -19,8 +26,6 @@ func _ready() -> void:
 	
 	ancien_mouse_mode = Input.mouse_mode
 	Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
-	
-	
 
 func continuer():
 	if FAIT_PAUSE :
@@ -51,3 +56,20 @@ func _on_recommencer_button_up() -> void:
 
 func _on_quitter_button_up() -> void:
 	get_tree().quit()
+
+
+
+func _on_options_button_up() -> void:
+	ouvrir_options()
+	menu_central.process_mode = Node.PROCESS_MODE_DISABLED
+
+func ouvrir_options():
+	if not inst_options :
+		inst_options = scene_options.instantiate()
+		add_child(inst_options)
+
+func fermer_options():
+	menu_central.process_mode = Node.PROCESS_MODE_INHERIT
+	if inst_options :
+		inst_options.queue_free()
+		inst_options = null
