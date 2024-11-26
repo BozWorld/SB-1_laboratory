642b6e8f9b14e282b3a0b78ad0cc505aaf7c4e8d
Sammouille <77251950+Sammouille@users.noreply.github.com>
2024-11-26 14:31:54 +0100

    contours plus jolis

--- Scene/CollisionCercleVisible.gd
+++ Scene/CollisionCercleVisible.gd
@@ -14,6 +14,7 @@ func _ready() -> void:
 
 func _draw() -> void:
 	draw_circle(Vector2(0,0),rayon,Color(1.0,1.0,1.0))
+	draw_circle(Vector2(0,0),rayon,Color(0.0,0.0,0.0), false, 4.0)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -27,7 +28,7 @@ func _process(delta: float) -> void:
 		shape.radius = rayon
 		
 	else :
-		modulate.a -= delta*0.3
+		modulate.a -= delta*0.5
 	
 	
 	
