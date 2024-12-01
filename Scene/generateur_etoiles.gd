ca54e4eb6fc52327c8444986c474d6704a55e489
Sammouille <77251950+Sammouille@users.noreply.github.com>
2024-12-01 02:44:00 +0100

    Je sais plus

---
+++ Scene/generateur_etoiles.gd
@@ -0,0 +1,16 @@
+extends Node2D
+
+@export var nombre_etoile : int = 10
+
+@export var debut_range : Vector2 = Vector2(0,0)
+
+@export var fin_range : Vector2 = Vector2(1152,648)
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
