d55bfb7d03b76b1a19a55cf46d1a2d596d0f71f1
Sammouille <77251950+Sammouille@users.noreply.github.com>
2025-05-31 16:58:38 +0200

    Creation Boite a outils

--- BoiteOutils/CycleJourNuit/jour_nuit.gd
+++ BoiteOutils/CycleJourNuit/jour_nuit.gd
@@ -1,8 +1,10 @@
-extends DirectionalLight3D
+extends Node3D
 
+
+@export var vitesse = 4.4
 var doDaylightCycle = true
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if doDaylightCycle :
-		rotation.x += 0.01 * delta
+		$DirectionalLight3D.rotation.x += 0.01 * delta * vitesse
