5693847f5cf5b843b02c36ecec2491adc0073ac2
Sammouille <77251950+Sammouille@users.noreply.github.com>
2025-05-31 16:38:23 +0200

    tjr le passage en 4.4 et tout

--- ScriptEtScenes/CycleJourNuit.gd
+++
@@ -1,8 +0,0 @@
-extends DirectionalLight3D
-
-var doDaylightCycle = true
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	if doDaylightCycle :
-		rotation.x += 0.01 * delta
