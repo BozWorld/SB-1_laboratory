220e9e16efcd385e87659cd0ba45a34a0940060f
Sammouille <77251950+Sammouille@users.noreply.github.com>
2025-05-31 17:00:11 +0200

    Rangement Boite

--- Script/ActeursBasiques/randonnereuse_disco.gd
+++
@@ -1,8 +0,0 @@
-extends Randonneureuse
-
-func _pas():
-	super()
-	var choix_r = randf()
-	var choix_v = randf()
-	var choix_b = randf()
-	mesh.material.albedo_color = Color(choix_r, choix_v, choix_b)
