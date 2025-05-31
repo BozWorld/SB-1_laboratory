3cc12b76ff6fe933863cf1554c444832353e5a83
Sammouille <77251950+Sammouille@users.noreply.github.com>
2025-05-31 16:38:06 +0200

    Passage en 4.4 + uids

---
+++ Sesh/ScriptEtScenes/CycleJourNuit.gd
@@ -0,0 +1,8 @@
+extends DirectionalLight3D
+
+var doDaylightCycle = true
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if doDaylightCycle :
+		rotation.x += 0.01 * delta
---
+++ Sesh/ScriptEtScenes/perso/attributs/frottement_fluide.gd
@@ -0,0 +1,17 @@
+extends Attribut3D
+
+var coef : float = 0.1
+var surface : float = 1.0
+var densite : float = 1.0
+
+func _frottement_process(delta, velocite : Vector3):
+	var frot_direction = -velocite.normalized()
+	
+	var frot_mag = velocite.length() * velocite.length() * coef * surface * densite * delta
+	
+	var frottement = frot_direction * frot_mag
+	parent.appliquer_force(frottement)
+	#print("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+	#Frottement Fluide : 
+	#Direction = " + str(frot_direction) + "
+	#Magnitude = " + str(frot_mag))
