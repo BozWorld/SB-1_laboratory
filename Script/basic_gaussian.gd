d025b561aba8d73a4781f666080389ed51fb10f1
Boz <mondohamoeva@hotmail.fr>
2025-07-08 11:53:32 +0200

    { Basic Gaussian }

--- Script/basic_gaussian.gd
+++ Script/basic_gaussian.gd
@@ -6,9 +6,9 @@ var color := Color(1, 0, 0, 0.05) # couleur rouge avec alpha faible
 func _process(_delta):
 	var x = randfn(get_viewport().get_visible_rect().size.x/2, ecart_type)
 	var y = randfn(get_viewport().get_visible_rect().size.y/2, ecart_type)
-	var hue = randfn(0.1,0.05)
-	var saturation = randfn(0.2, 0.1)
-	var value = randfn(0.4, 0.1)
+	var hue = randfn(0.5,0.05)
+	var saturation = randfn(0.8, 0.1)
+	var value = randfn(0.2, 0.1)
 	color = Color.from_hsv(hue, saturation, value, 0.05)
 	points.append({
 		"pos": Vector2(x, y),
