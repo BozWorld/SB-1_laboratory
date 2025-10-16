267f2ca62eb0baf2afcbc9fc4e1ef37344a3847a
Sammouille <77251950+Sammouille@users.noreply.github.com>
2025-10-16 17:28:27 +0200

    c bon

--- code/effect/Trail.gd
+++ code/effect/Trail.gd
@@ -5,7 +5,7 @@ class_name Trail
 
 var min_trail_speed: float = 8.0
 
-
+@export var resolution_cylindre := 5
 # === PARAMÈTRE DE TRAIL
 @export var trail_enabled: bool = true
 @export var trail_width_start: float = 0.3
@@ -16,17 +16,23 @@ var min_trail_speed: float = 8.0
 
 # === DONNÉES INTERNES ===
 var _points: Array[Vector3] = []
+var _basis: Array[Basis] = []
 var _lifetimes: Array[float] = []
 var _directions: Array[Vector3] = []
 var _last_position: Vector3
+var inv_cylindre : float
+
 
 # === COULEURS ===
 @export var color_start: Color = Color(0.9,0.9,1.0,0.8)
 @export var color_end: Color = Color(0.5, 0.5, 0.8, 0.0)
 
+
+
 func setup():
 	mesh = ImmediateMesh.new()
 	_last_position = global_position
+	inv_cylindre = 2.0 / resolution_cylindre
 
 func update_trail(speed: float, grounded: bool, delta: float):
 	if !trail_enabled:
@@ -48,6 +54,7 @@ func _update_points_lifetime(delta: float):
 		_lifetimes[i] += delta
 		if _lifetimes[i] >= trail_lifetime:
 			_points.remove_at(i)
+			_basis.remove_at(i)
 			_lifetimes.remove_at(i)
 			_directions.remove_at(i)
 		else:
@@ -56,20 +63,22 @@ func _update_points_lifetime(delta: float):
 func _try_add_point():
 	var current_pos = global_position
 	if (_last_position - current_pos).length() > trail_precision:
-		_add_point(current_pos)
+		_add_point()
 		_last_position = current_pos
 
-func _add_point(position: Vector3):
+func _add_point():
 	var direction = Vector3.FORWARD
 	if _points.size() > 0:
-		direction = (position - _points[-1]).normalized()
-	
-	_points.append(position)
+		direction = (global_position - _points[-1]).normalized()
+		
+	_points.append(global_position)
+	_basis.append(global_basis)
 	_directions.append(direction)
 	_lifetimes.append(0.0)
 
 func _clear_trail():
 	_points.clear()
+	_basis.clear()
 	_lifetimes.clear()
 	_directions.clear()
 	if mesh:
@@ -109,24 +118,97 @@ func _create_quad_segment(index: int, width1: float, width2: float, color1: Colo
 	if right.length() < 0.1:
 		right = Vector3.RIGHT
 	
-	var p1_left = pos1 - right * width1 * 0.5
-	var p1_right = pos1 + right * width1 * 0.5
-	var p2_left = pos2 - right * width2 * 0.5
-	var p2_right = pos2 + right * width2 * 0.5
 
-	mesh.surface_set_color(color1)
-	mesh.surface_add_vertex(p1_left)
-	mesh.surface_set_color(color2)
-	mesh.surface_add_vertex(p2_left)
-	mesh.surface_set_color(color1)
-	mesh.surface_add_vertex(p1_right)
+	if resolution_cylindre < 2 :
+			
 
-	mesh.surface_set_color(color1)
-	mesh.surface_add_vertex(p1_right)
-	mesh.surface_set_color(color2)
-	mesh.surface_add_vertex(p2_left)
-	mesh.surface_set_color(color2)
-	mesh.surface_add_vertex(p2_right)
+		var p1_left = pos1 - right * width1 * 0.5
+		var p1_right = pos1 + right * width1 * 0.5
+		var p2_left = pos2 - right * width2 * 0.5
+		var p2_right = pos2 + right * width2 * 0.5
+
+		mesh.surface_set_color(color1)
+		mesh.surface_add_vertex(p1_left)
+		mesh.surface_set_color(color2)
+		mesh.surface_add_vertex(p2_left)
+		mesh.surface_set_color(color1)
+		mesh.surface_add_vertex(p1_right)
+
+		mesh.surface_set_color(color1)
+		mesh.surface_add_vertex(p1_right)
+		mesh.surface_set_color(color2)
+		mesh.surface_add_vertex(p2_left)
+		mesh.surface_set_color(color2)
+		mesh.surface_add_vertex(p2_right)
+
+	else :
+		
+
+		var basis1 = _basis[index]
+		var basis2 = _basis[index+1]
+
+		var points_cylindre1 : Array[Vector3] = []
+		var points_cylindre2 : Array[Vector3] = []
+
+		for i in range(resolution_cylindre) :
+			var rota = i * inv_cylindre
+			print(rota)
+
+			var nouveau_point1 = basis1.x * width1 * 0.5
+			nouveau_point1 = nouveau_point1.rotated(basis1.y, rota * PI)
+			points_cylindre1.append(nouveau_point1)
+			
+			var nouveau_point2 = basis2.x * width2 * 0.5
+			nouveau_point2 = nouveau_point2.rotated(basis2.y, rota * PI)
+			points_cylindre2.append(nouveau_point2)
+
+		for i in range(resolution_cylindre):
+
+			if i != 0 :
+				# Left et right dans le referentiel de l'axe du point du cylindre
+				var p1_left = pos1 + points_cylindre1[i]
+				var p1_right = pos1 + points_cylindre1[i-1]
+				var p2_left = pos2 + points_cylindre2[i]
+				var p2_right = pos2 + points_cylindre2[i-1]
+
+				mesh.surface_set_color(color1)
+				mesh.surface_add_vertex(p1_left)
+				mesh.surface_set_color(color2)
+				mesh.surface_add_vertex(p2_left)
+				mesh.surface_set_color(color1)
+				mesh.surface_add_vertex(p1_right)
+
+				mesh.surface_set_color(color1)
+				mesh.surface_add_vertex(p1_right)
+				mesh.surface_set_color(color2)
+				mesh.surface_add_vertex(p2_left)
+				mesh.surface_set_color(color2)
+				mesh.surface_add_vertex(p2_right)
+			else :
+				
+				# Left et right dans le referentiel de l'axe du point du cylindre
+				var p1_left = pos1 + points_cylindre1[i]
+				var p1_right = pos1 + points_cylindre1[resolution_cylindre - 1]
+				var p2_left = pos2 + points_cylindre2[i]
+				var p2_right = pos2 + points_cylindre2[resolution_cylindre - 1]
+
+				mesh.surface_set_color(color1)
+				mesh.surface_add_vertex(p1_left)
+				mesh.surface_set_color(color2)
+				mesh.surface_add_vertex(p2_left)
+				mesh.surface_set_color(color1)
+				mesh.surface_add_vertex(p1_right)
+
+				mesh.surface_set_color(color1)
+				mesh.surface_add_vertex(p1_right)
+				mesh.surface_set_color(color2)
+				mesh.surface_add_vertex(p2_left)
+				mesh.surface_set_color(color2)
+				mesh.surface_add_vertex(p2_right)
+
+
+
+
 
 func set_trail_enabled(enabled: bool):
 	trail_enabled = enabled
