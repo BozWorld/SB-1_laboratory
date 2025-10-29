87145e0069e8fc3a50dcc264583c78f3d01b7bb3
Sammouille <samuel.wolf@hotmail.fr>
2025-10-29 15:23:26 +0100

    Amélioration suiveur

--- Scene/Vaisseau/suiveur.gd
+++ Scene/Vaisseau/suiveur.gd
@@ -2,8 +2,8 @@ extends Node3D
 
 @export var cible : Node3D
 
-@export_range(0.0,10000.0,0.1) var vitesse_deplacement : float = 10.0
-@export_range(0.0,100.0,0.1) var vitesse_rotation : float = 0.2
+@export_range(0.0,1.0,0.01) var vitesse_deplacement : float = 0.3
+@export_range(0.0,1.0,0.01) var vitesse_rotation : float = 0.2
 
 var latence := 0.0
 
@@ -11,8 +11,8 @@ var latence := 0.0
 func _physics_process(delta):
 	if cible :
 		if cible.position != position :
+			position = position.slerp(cible.position, vitesse_deplacement)
 			latence = (cible.position-position).length()
-			position += (cible.position - position) * vitesse_deplacement * delta
 		if cible.transform.basis != transform.basis :
 			var rota = Quaternion(transform.basis.orthonormalized())
 			var rota_cible = Quaternion(cible.transform.basis.orthonormalized())
